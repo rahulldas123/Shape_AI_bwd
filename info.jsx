9d4bb423c9ed4a56e0421a34429ac2ce612f7896
rahulldas123 <53289157+rahulldas123@users.noreply.github.com>
2021-06-28 07:55:17 +0530

    Info.js instead of note.jsx is used

---
+++ info.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+function Info(){
+  return(
+    <div className="note">
+     <h1> Javascript and Reactj.s</h1>
+     <p> A bootcamp on the basics of Javascript and React.js for web development</p>
+  </div>
+  );
+
+}
+
+export default Info;
